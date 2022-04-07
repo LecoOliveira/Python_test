f1d96167c5e39483980601d3ef04d289a9b063a1
LecoOliveira <lecoverde10@gmail.com>
2022-04-07 18:28:54 -0300

    clienttcp.py

--- clienttcp.py
+++ clienttcp.py
@@ -1,15 +1,23 @@
 import socket
+import sys
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0)
 client.settimeout(2)
 
-try:
-    client.connect(("google.com", 80))
+def main():
+    try:
+        client.connect(("google.com", 80))
 
-    client.send("GET / HTTP/1.1\nHost: google.com\n\n") 
+        client.send("GET / HTTP/1.1\nHost: google.com\n\n") 
 
-    pacotes_recebidos = client.recv(1204) 
+        pacotes_recebidos = client.recv(1204) 
 
-    print pacotes_recebidos
-except:
-    print "Falha na autenticacao"
+        print pacotes_recebidos
+
+    except socket.error as error:
+        print "Falha na autenticacao"
+        print "Erro: {}".format(error)
+        sys.exit
+
+if __name__ == "__main__":
+    main()
