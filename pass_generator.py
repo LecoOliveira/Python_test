b21e5f98fb91279b22c371288c84230c015d4cd6
LecoOliveira <lecoverde10@gmail.com>
2022-04-07 20:24:34 -0300

    .

--- pass_generator.py
+++ pass_generator.py
@@ -4,8 +4,8 @@
 
 import random, string
 
-size = input("Type the size of your pass: ")
+size = input("Enter your password length: ")
 chars = string.ascii_letters + string.digits + '!@#$%*'
 rnd = random.SystemRandom()
 
-print 'Pass: '.join(rnd.choice(chars) for i in range(size))
+print ''.join(rnd.choice(chars) for i in range(size))
