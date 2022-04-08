ce85e665005b3b2dcf46651cc9fab9058fa60807
LecoOliveira <lecoverde10@gmail.com>
2022-04-08 18:27:33 -0300

    .

---
+++ hash_generator.py
@@ -0,0 +1,139 @@
+#!/usr/bin/python3
+
+import base64
+import hashlib
+import os
+import time
+
+def clear():
+
+    if os.name == "nt":
+        os.system("cls")
+    else:
+        os.system("clear")
+
+def title():
+    print('''\n============ CHOOSE HASH TYPE ============
+[1] SHA1        [3] MD5         [5] Base64
+[2] SHA256      [4] SHA512      [6] Exit
+==========================================\n''')
+
+def main():
+
+    clear()
+    while True:
+        string = input("Type the text to be encoded: ")
+        title()
+        options = int(input("\nSelect the option: "))
+        
+        clear()
+        time.sleep(1)
+        if options < 1 or options > 6:
+            print("Invalid option\n")
+            continue
+
+        elif options == 1:
+            result = hashlib.sha1(string.encode('utf-8'))
+            print(f"\nSHA1: {result.hexdigest()}")
+            print()
+            repeat = input("Do you want to encript another text (y/n)? ")
+            if repeat.lower() == 'y':
+                clear()
+                continue
+            elif repeat.lower() == 'n':
+                print()
+                time.sleep(0.5)
+                print("End Program.")
+                break
+            else:
+                print()
+                print("Invalid option.")
+                break
+
+        elif options == 2:
+            result = hashlib.sha256(string.encode('utf-8'))
+            print(f"\nSHA256: {result.hexdigest()}")
+            print()
+            repeat = input("Do you want to encript another text (y/n)? ")
+            if repeat.lower() == 'y':
+                clear()
+                continue
+            elif repeat.lower() == 'n':
+                print()
+                time.sleep(0.5)
+                print("End Program.")
+                break
+            else:
+                print()
+                print("Invalid option.")
+                break
+
+        elif options == 3:
+            result = hashlib.md5(string.encode('utf-8'))
+            print(f"\nMD5: {result.hexdigest()}")
+            print()
+            repeat = input("Do you want to encript another text (y/n)? ")
+            if repeat.lower() == 'y':
+                clear()
+                continue
+            elif repeat.lower() == 'n':
+                print()
+                time.sleep(0.5)
+                print("End Program.")
+                break
+            else:
+                print()
+                print("Invalid option.")
+                break
+
+        elif options == 4:
+            result = hashlib.sha512(string.encode('utf-8'))
+            print(f"\nSHA512: {result.hexdigest()}")
+            print()
+            repeat = input("Do you want to encript another text (y/n)? ")
+            if repeat.lower() == 'y':
+                clear()
+                continue
+            elif repeat.lower() == 'n':
+                print()
+                time.sleep(0.5)
+                print("End Program.")
+                break
+            else:
+                print()
+                print("Invalid option.")
+                break
+
+        elif options == 5:
+            result = base64.b64encode(string.encode('utf-8'))
+            result_encoded = result.decode('utf-8')
+            print(f"\nBase64: {result_encoded}")
+            print()
+            repeat = input("Do you want to encript another text (y/n)? ")
+            if repeat.lower() == 'y':
+                clear()
+                continue
+            elif repeat.lower() == 'n':
+                print()
+                time.sleep(0.5)
+                print("End Program.")
+                break
+            else:
+                print()
+                print("Invalid option.")
+                break
+
+        elif options == 6:
+            break
+
+if __name__ == '__main__':
+    try:
+        main()
+    except ValueError:
+        print("\nInvalid character. Type a number between 1 and 6.")
+    except Exception as error:
+        print(f"\nError: {str(error)}")
+    except KeyboardInterrupt:
+        time.sleep(0.5)
+        print("\n\nEnd program!")
+    
