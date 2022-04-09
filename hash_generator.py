e36e179a0567cdd1e9084b37f5c21801b8e1dd7a
LecoOliveira <lecoverde10@gmail.com>
2022-04-09 15:09:05 -0300

    improving

--- hash_generator.py
+++ hash_generator.py
@@ -10,12 +10,14 @@ import hashlib
 import os
 import time
 
+
 def clear():
 
-    if os.name == "nt":
-        os.system("cls")
+    if os.name == 'nt':
+        os.system('cls')
     else:
-        os.system("clear")
+        os.system('clear')
+
 
 def title():
     print('''\n============ CHOOSE HASH TYPE ============
@@ -23,129 +25,103 @@ def title():
 [2] SHA256      [4] SHA512      [6] Exit
 ==========================================\n''')
 
+
+def cont():
+    key = True
+    repeat = input('Do you want to encript another text (y/n)? ')
+    if repeat.lower() == 'y':
+        clear()
+        return key
+    elif repeat.lower() == 'n':
+        print()
+        time.sleep(0.5)
+        print('End Program.')
+        key = False
+    else:
+        print()
+        print('Invalid option.')
+        key = False
+
+
 def main():
 
     clear()
     while True:
-        string = input("Type the text to be encoded: ")
+        string = input('Type the text to be encoded: ')
         title()
-        options = int(input("\nSelect the option: "))
+        options = int(input('\nSelect the option: '))
         
         time.sleep(1)
-        if options < 1 or options > 6:
-            print("Invalid option\n")
-            continue
+        while options < 1 or options > 6:
+            print('\nInvalid option')
+            options = int(input('Type a number between 1 and 6: '))
 
-        elif options == 1:
+        if options == 1:
             clear()
             result = hashlib.sha1(string.encode('utf-8'))
-            print(f"\nSHA1: {result.hexdigest()}")
+            print(f'\nSHA1: {result.hexdigest()}')
             print()
-            repeat = input("Do you want to encript another text (y/n)? ")
-            if repeat.lower() == 'y':
-                clear()
+            if cont():
                 continue
-            elif repeat.lower() == 'n':
-                print()
-                time.sleep(0.5)
-                print("End Program.")
-                break
             else:
-                print()
-                print("Invalid option.")
-                break
+                break           
 
         elif options == 2:
             clear()
             result = hashlib.sha256(string.encode('utf-8'))
-            print(f"\nSHA256: {result.hexdigest()}")
+            print(f'\nSHA256: {result.hexdigest()}')
             print()
-            repeat = input("Do you want to encript another text (y/n)? ")
-            if repeat.lower() == 'y':
-                clear()
+            if cont():
                 continue
-            elif repeat.lower() == 'n':
-                print()
-                time.sleep(0.5)
-                print("End Program.")
-                break
             else:
-                print()
-                print("Invalid option.")
                 break
 
         elif options == 3:
             clear()
             result = hashlib.md5(string.encode('utf-8'))
-            print(f"\nMD5: {result.hexdigest()}")
+            print(f'\nMD5: {result.hexdigest()}')
             print()
-            repeat = input("Do you want to encript another text (y/n)? ")
-            if repeat.lower() == 'y':
-                clear()
+            if cont():
                 continue
-            elif repeat.lower() == 'n':
-                print()
-                time.sleep(0.5)
-                print("End Program.")
-                break
             else:
-                print()
-                print("Invalid option.")
                 break
 
         elif options == 4:
             clear()
             result = hashlib.sha512(string.encode('utf-8'))
-            print(f"\nSHA512: {result.hexdigest()}")
+            print(f'\nSHA512: {result.hexdigest()}')
             print()
-            repeat = input("Do you want to encript another text (y/n)? ")
-            if repeat.lower() == 'y':
-                clear()
+            if cont():
                 continue
-            elif repeat.lower() == 'n':
-                print()
-                time.sleep(0.5)
-                print("End Program.")
-                break
             else:
-                print()
-                print("Invalid option.")
                 break
 
         elif options == 5:
             clear()
             result = base64.b64encode(string.encode('utf-8'))
             result_encoded = result.decode('utf-8')
-            print(f"\nBase64: {result_encoded}")
+            print(f'\nBase64: {result_encoded}')
             print()
-            repeat = input("Do you want to encript another text (y/n)? ")
-            if repeat.lower() == 'y':
-                clear()
+            if cont():
                 continue
-            elif repeat.lower() == 'n':
-                print()
-                time.sleep(0.5)
-                print("End Program.")
-                break
             else:
-                print()
-                print("Invalid option.")
                 break
 
         elif options == 6:
-            print("End program.")
+            print('End program.')
             time.sleep(1)
             clear()
             break
+        
 
 if __name__ == '__main__':
     try:
         main()
     except ValueError:
-        print("\nInvalid character. Type a number between 1 and 6.")
+        print('\nInvalid character. Type a number between 1 and 6.')
     except Exception as error:
-        print(f"\nError: {str(error)}")
+        print(f'\nError: {str(error)}')
     except KeyboardInterrupt:
         time.sleep(0.5)
-        print("\n\nEnd program!")
+        print('\n\nEnd program!')
     
