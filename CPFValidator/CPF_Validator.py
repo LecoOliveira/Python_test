57c455cbe15703c082e58c5f6c79838e48faa90f
LecoOliveira <lecoverde10@gmail.com>
2022-05-02 20:25:46 -0300

    .

--- CPFValidator/CPF_Validator.py
+++ CPFValidator/CPF_Validator.py
@@ -1,7 +1,26 @@
 from calculo import *
-    
-cpf_user = int(input('Digite seu CPF: '))
-cpf = [int(x) for x in str(cpf_user)] 
-*cpf_usavel, digito_1, digito_2 = cpf
 
-print('CPF Válido!') if calculo_digito1(cpf_usavel) == digito_1 and calculo_digito2(cpf_usavel) == digito_2 else print('CPF Inválido!')
+tentativa = 3
+
+while True:
+   
+    try:
+        cpf_user = int(input('Digite seu CPF: '))
+        cpf = [int(x) for x in str(cpf_user)] 
+        *cpf_usavel, digito_1, digito_2 = cpf
+
+        print('CPF Válido!') if calculo_digito1(cpf_usavel) == digito_1 and calculo_digito2(cpf_usavel) == digito_2 else print('CPF Inválido!')
+        break
+    
+    except Exception as erro:
+        print(f'Erro: {erro}\n')
+        tentativa -= 1
+        if tentativa == 0:
+            print('Tentativas excedidas. Inicie o programa novamente.')
+            break
+        else:
+            continue
+
+    except KeyboardInterrupt:
+        print('\nFim do programa. \nObrigado!')
+        break
