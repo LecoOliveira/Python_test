b439b0fd2e61bc5afd4f630128d76a9ae3760cc4
LecoOliveira <lecoverde10@gmail.com>
2022-05-16 00:01:16 -0300

    refactoring

--- HashGenerator/hash_generator.py
+++ HashGenerator/hash_generator.py
@@ -8,30 +8,6 @@
 from convert import *
 from functions import *
 
-def choose_conversion(string):
-
-    options = int(input('Select the option: '))
-    sleep(1)
-
-    while options < 1 or options > 6:
-        print('\nInvalid option')
-        options = int(input('Type a number between 1 and 6: '))
-    
-    if options == 6:
-        print('End program.')
-        sleep(1)
-        clear()
-        return exit()
-
-    convert = {
-        1: lambda string: sha1_convert(string),
-        2: lambda string: sha256_convert(string),
-        3: lambda string: sha512_convert(string),
-        4: lambda string: md5_convert(string),
-        5: lambda string: base64_convert(string)
-    }
-    return convert[options](string)
-
 
 def main():
 
